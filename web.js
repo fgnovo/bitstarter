b1d3c5967da4ff7434185f2c5f64d63c6a94d3a2
Fernando Garcia <fgnovo@gmail.com>
2013-07-15 17:12:34 +0000

    Web.js wirh index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    fs.readSync('./index.html','r',function(data){
+	var string = data.toString();
+	response.send(string);
+    });
 });
 
 var port = process.env.PORT || 5000;
